2f6ed57f514b8d8142aeb3dedc8d62e46acadf48
miguelgargs <miguelgargs@gmail.com>
2026-09-01 19:29:55 +0200

    halfway through trying to understand how to compute the checksum
    - i might need to do this in a different file because the project is
      getting dirty and the setup is complex

---
+++ src/checksum.rs
@@ -0,0 +1,18 @@
+/// Calculates the CRC16_MCRF4XX checksum over a byte buffer.
+///
+/// # Arguments
+///
+/// * `buffer` - The bytes to hash.
+/// * `length` - Length of the byte array.
+///
+/// # Returns
+/// * `u16` calculated checksum
+/// The checksum over the buffer bytes
+pub fn crc_calculate(buffer: u8, length: u16) -> u16 {
+    let mut crc_tmp: u16;
+    // initialize crc_tmp to empty
+    crc_tmp = 0xffff;
+
+    println!("{}", crc_tmp);
+    crc_tmp
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod checksum;
+
 fn main() {
-    println!("Hello, world!");
+    let byte_string = "123456789";
+    let crc = checksum::crc_calculate(byte_string, byte_string.chars().count());
+    println!("0x{:04X}", crc);
 }
