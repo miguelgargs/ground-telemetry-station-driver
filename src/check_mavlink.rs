91a7abc6e18b3a53e6ac447c489f500ef4c9287d
miguelgargs <miguelgargs@gmail.com>
2026-09-06 13:22:29 +0200

    update check_mavlink function with a match clause
    - tests keep passing

--- src/check_mavlink.rs
+++ src/check_mavlink.rs
@@ -2,7 +2,12 @@ pub fn is_mavlink(msg: &[u8]) -> bool {
     // read the header of the received message to check if it's a mavlink message
     let first_byte = msg[0];
     println!("The first byte in the msg is: {first_byte:x?}");
-    return first_byte == 0xFD;
+    // return first_byte == 0xFD;
+    match first_byte {
+        0xFD => return true,
+        0xFE => return true,
+        _ => false,
+    }
 }
 
 #[cfg(test)]
@@ -11,6 +16,7 @@ mod tests {
 
     #[test]
     fn test_check_right() {
+        // Check valid MAVLink header byte
         let mut buf: [u8; 2] = [0; 2];
         buf[0] = 0xFD;
         assert_eq!(is_mavlink(&buf), true);
@@ -20,6 +26,13 @@ mod tests {
     fn test_check_wrong() {
         let mut buf: [u8; 2] = [0, 2];
         buf[0] = 0x00;
+        assert_eq!(is_mavlink(&buf), false);
+    }
+
+    #[test]
+    fn test_check_wrong_2() {
+        let mut buf: [u8; 2] = [0, 2];
+        buf[0] = 0x12;
         assert_eq!(is_mavlink(&buf), false);
     }
 }
