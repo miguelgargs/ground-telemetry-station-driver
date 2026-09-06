90e46f37c14bb424a0f71886672dd3b5d4d26ecc
miguelgargs <miguelgargs@gmail.com>
2026-09-06 19:23:33 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,19 @@ mod check_mavlink;
 fn main() {
     // rust is nice:)
     println!("Starting the program...");
-    let mut stream = TcpStream::connect("127.0.0.1:5760").expect("Could not create socket");
-    println!("Socket is open on 127.0.0.1:5760");
+    let uri = String::from("127.0.0.1");
+    let port = 5760;
+    let mut endpoint = String::from("");
+    endpoint.push_str(&uri);
+    endpoint.push_str(":");
+    endpoint.push_str(&port.to_string());
+    println!("[INFO] Endpoint is: {endpoint}");
+    let mut stream = TcpStream::connect(&endpoint).expect("Could not create socket");
+    println!("Socket is open on {endpoint}");
     let mut buf: [u8; 560] = [0; 560]; // buffer that fits two mavlink messages (max length is 280B)
     loop {
         println!("Waiting for data...");
-        let read_bytes = stream.read(&mut buf);
+        let read_bytes = stream.read(&mut buf); // buffer is overwritten every time a new message arrives
 
         println!("Received {read_bytes:?} bytes from TcpSocket!!"); // :? is for pretty-printing :)
         //println!("Buffer contents: {buf:?}");
